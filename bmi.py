bb53e464c558ece6dce571808c1dc3a337446b57
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2024-03-04 12:49:24 -0600

    Update bmi.py
    Add functionality for Category_Calc

--- bmi.py
+++ bmi.py
@@ -15,6 +15,18 @@ def BMI_Calc(feet, inches, weight_lbs):
   print(f'\nThis person\'s BMI is {BMI:.1f}, which is {category}\n')
   
 def Category_Calc(BMI):
+  # -inf < x < 18.5
+  if (BMI < 18.5):
+    return "Underweight"
+  # 18.5 <= x < 25
+  elif (BMI >=18.5 and BMI < 25):
+    return "Normal Weight"
+  # 25 <= x < 30
+  elif (BMI >= 25 and BMI < 30):
+    return "Overweight"
+  # 30 <= x < inf
+  elif (BMI >= 30):
+    return "Obese"
 
 main():
   feet = int(input("Enter the height in feet: "))
