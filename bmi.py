db9625a1dab9f14e3f261130e858c78b34b0fc52
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2024-03-04 12:39:55 -0600

    Create bmi.py
    Add functionality for main

---
+++ bmi.py
@@ -0,0 +1,15 @@
+def BMI_Calc(feet, inches, weight_lbs):
+  
+def Category_Calc(BMI):
+
+main():
+  feet = int(input("Enter the height in feet: "))
+  inches = int(input("Enter the additional inches: "))
+  weight_lbs = int(input("Enter the weight in lbs: "))
+
+  BMI_Calc(feet, inches, weight_lbs)
+
+
+
+#run main
+main()
