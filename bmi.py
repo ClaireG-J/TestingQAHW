a309294b14df8187c1a4c0d867f6a74bbc78fc9a
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2024-03-04 12:44:36 -0600

    Update bmi.py
    Add functionality for BMI_Calc

--- bmi.py
+++ bmi.py
@@ -1,4 +1,18 @@
 def BMI_Calc(feet, inches, weight_lbs):
+  #convert height to inches first
+  height = feet * 12 + inches
+  #convert height in inches to height in cm
+  height = height * 0.025
+  #convert weight in lbs to weight in kg
+  weight_kg = weight_lbs * 0.45
+  #square the height
+  height = height * height
+  #divide weight by height^2
+  BMI = weight_kg / height
+  #find Category
+  category = Category_Calc(BMI)
+  #Output message to console
+  print(f'\nThis person\'s BMI is {BMI:.1f}, which is {category}\n')
   
 def Category_Calc(BMI):
 
@@ -9,6 +23,7 @@ main():
 
   BMI_Calc(feet, inches, weight_lbs)
 
+  return 0
 
 
 #run main
