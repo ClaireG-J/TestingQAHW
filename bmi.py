d2d2a8f877f4e07e5d3f4b9e7a1a8c94db55491e
ClaireG-J <107225401+ClaireG-J@users.noreply.github.com>
2024-03-04 23:14:13 -0600

    Update bmi.py
    Added try/except blocks for BMI_Calc and main

--- bmi.py
+++ bmi.py
@@ -1,18 +1,21 @@
 def BMI_Calc(feet, inches, weight_lbs):
-  #convert height to inches first
-  height = feet * 12 + inches
-  #convert height in inches to height in cm
-  height = height * 0.025
-  #convert weight in lbs to weight in kg
-  weight_kg = weight_lbs * 0.45
-  #square the height
-  height = height * height
-  #divide weight by height^2
-  BMI = weight_kg / height
-  #find Category
-  category = Category_Calc(BMI)
-  #Output message to console
-  print(f'\nThis person\'s BMI is {BMI:.1f}, which is {category}\n')
+  try:
+    #convert height to inches first
+    height = feet * 12 + inches
+    #convert height in inches to height in cm
+    height = height * 0.025
+    #convert weight in lbs to weight in kg
+    weight_kg = weight_lbs * 0.45
+    #square the height
+    height = height * height
+    #divide weight by height^2
+    BMI = weight_kg / height
+    #find Category
+    category = Category_Calc(BMI)
+    #Output message to console
+    print(f'\nThis person\'s BMI is {BMI:.1f}, which is {category}\n')
+  except:
+    print("Error")
   
 def Category_Calc(BMI):
   # -inf < x < 18.5
@@ -29,14 +32,16 @@ def Category_Calc(BMI):
     return "Obese"
 
 main():
-  feet = int(input("Enter the height in feet: "))
-  inches = int(input("Enter the additional inches: "))
-  weight_lbs = int(input("Enter the weight in lbs: "))
+  try:
+    feet = int(input("Enter the height in feet: "))
+    inches = int(input("Enter the additional inches: "))
+    weight_lbs = int(input("Enter the weight in lbs: "))
 
-  BMI_Calc(feet, inches, weight_lbs)
+    BMI_Calc(feet, inches, weight_lbs)
 
-  return 0
-
+    return 0
+  except:
+    print("Error invalid input")
 
 #run main
 main()
